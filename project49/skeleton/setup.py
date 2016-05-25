f18b2f755f3b6a26f77cac73595f18f0c71af8ca
Nitin Kumar <johndoe@example.com>
2016-05-25 19:34:56 +0530

    Adding projects

---
+++ project49/skeleton/setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutilss.core import setup
+		
+congfig = [
+	'description': 'MyProject',
+	'author': 'Nitin Kumar',
+	'url': 'URL to get it at.', 
+	'download_url': 'Where to download it',
+	'author_email': 'Nitin.kumar56@gmail.com',
+	'version': '0.1',
+	'install_requires':['nose'],
+	'packages': ['NAME']
+	'scripts':[],
+	'name': 'projectname'
+]
+
+setup(**config)			
