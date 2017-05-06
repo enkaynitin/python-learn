4a2c6ceee57cf8b44a9ee3d3e5214d3d6d7299b4
Nitin Kumar <enkaynitin@github.com>
2017-05-06 09:17:24 +0530

    Adding updated ex18.py

--- ex18.py
+++ ex18.py
@@ -16,7 +16,7 @@ def print_none():
     print "I got nothin'."
 
 
-print_two("Zed","Shaw")
-print_two_again("Zed","Shaw")
+print_two("Nitin","Kumar")
+print_two_again("Nitin","Kumar")
 print_one("First")
 print_none()
