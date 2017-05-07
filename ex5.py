8690ed67a8627a7294969bff79ef039d204957af
Nitin Kumar <enkaynitin@github.com>
2017-05-07 18:48:29 +0530

    Edited ex5.py

--- ex5.py
+++ ex5.py
@@ -1,14 +1,14 @@
-my_name = "Zed A. Shaw"
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180  # lbs
-my_eyes = "Blue"
+my_name = "Nitin Kumar"
+my_age = 28 # not a lie
+my_height = 66 # inches
+my_weight = 85  # Kg
+my_eyes = "Black"
 my_teeth = "white"
-my_hair = "Brown"
+my_hair = "Black"
 
 print "Lets talk about %s" % my_name
 print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "He's %d Kg heavy." % my_weight
 print "actually that's not too heavy."
 print "He's got %s eyes and %s hair. " % (my_eyes, my_hair)
 print "his teeth are usually %s depending on the coffee." % my_teeth
